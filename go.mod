55f78c37352609757136a0c07f62a056557aeabb
Amritanshu05 <amritanshu0507@gmail.com>
2025-04-08 02:21:10 +0530

    .

--- go.mod
+++ go.mod
@@ -1,4 +1,5 @@
 module github.com/Amritanshu05/GO-Webserver
 
 go 1.24.1
- 
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -4,18 +4,21 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("hello world")
-	
-	godotenv.Load()
+
+	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
 
 	if portString == "" {
 		log.Fatal("PORT is not found")
 	}
+	router := chi.NewRouter()
 	fmt.Println("PORT:", portString)
 
 }
